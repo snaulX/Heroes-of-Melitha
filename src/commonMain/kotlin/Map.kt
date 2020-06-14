967b8f15c6404c620e153c1fe15330e7726c64f6
snaulX <alexandergunger666@gmail.com>
2020-06-14 15:44:29 +0300

    Try to create collision of boxes and change map

--- src/commonMain/kotlin/Map.kt
+++ src/commonMain/kotlin/Map.kt
@@ -4,6 +4,7 @@ import com.soywiz.klock.seconds
 import com.soywiz.kmem.clamp
 import com.soywiz.korev.Key
 import com.soywiz.korge.animate.waitStop
+import com.soywiz.korge.input.onKeyDown
 import com.soywiz.korge.input.onOut
 import com.soywiz.korge.input.onOver
 import com.soywiz.korge.scene.Scene
@@ -11,11 +12,14 @@ import com.soywiz.korge.tween.duration
 import com.soywiz.korge.tween.get
 import com.soywiz.korge.tween.tween
 import com.soywiz.korge.view.*
+import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.async.async
 import com.soywiz.korio.async.launch
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korio.lang.closeable
 import com.soywiz.korio.serialization.xml.readXml
+import com.soywiz.korma.geom.Point
 import kotlin.math.pow
 import kotlin.random.Random
 
@@ -23,16 +27,34 @@ class Map(val dependency: Dependency) : Scene() {
     val player
         get() = MainModule.hero
     private val speed = player.speed
+    private val curSpeed: Point = Point()
     private var dx = MapParser.player.x
     private var dy = MapParser.player.y
     private val sprite: Sprite
         get() = player.sprite
 
     override suspend fun Container.sceneInit() {
+        var blockLeft = false
+        var blockRight = false
+        var blockUp = false
+        var blockDown = false
+        val portal = Sprite(SpriteAnimation(resourcesVfs["images\\portals.png"].readBitmap(),
+                spriteWidth = 64,
+                spriteHeight = 64,
+                marginLeft = 0,
+                marginTop = 0,
+                columns = 3,
+                rows = 1,
+                offsetBetweenColumns = 0,
+                offsetBetweenRows = 0))
+        val boxes: MutableList<Image> = mutableListOf()
+        val crystals: MutableList<Image> = mutableListOf()
+        val mobs: MutableList<Image> = mutableListOf()
         val w: Double = views.actualWidth.toDouble()
         val h: Double = views.actualHeight.toDouble()
         position(dx + w/2, dy + h/2)
         val load = async {
+            views.clearColor = Colors.BLACK
             MapParser.parse(resourcesVfs["maps\\${MainModule.currentMap}.xml"].readXml())
             val stone = resourcesVfs["maps\\floor\\stone_floor${Random.nextInt(1, 5)}.png"]
                     .readBitmap()
@@ -49,57 +71,88 @@ class Map(val dependency: Dependency) : Scene() {
                 }
             }
             for (b in MapParser.boxes) {
-                image(box) {
+                boxes.add(image(box) {
                     xy(b.x, b.y)
-                }
+                    addUpdater {
+                        blockDown = false
+                        blockLeft = false
+                        blockRight = false
+                        blockUp = false
+                        if (collidesWith(sprite)) {
+                            println("" + sprite.x + ", " + sprite.y)
+                            println(hitTest(400, 600))
+                            if (sprite.y - sprite.height <= y)
+                                    //&& hitTest(sprite.x, sprite.y - sprite.height) != null)
+                                blockDown = true
+                            if (sprite.y >= y - height) blockUp = true
+                            if (sprite.x - sprite.width <= x) blockRight = true
+                            if (sprite.x >= x - width) blockLeft = true
+                        }
+                    }
+                })
             }
             for (c in MapParser.crystals) {
-                image(crystal) {
+                crystals.add(image(crystal) {
                     xy(c.x, c.y)
-                }
+                })
             }
             for (g in MapParser.goblins) {
-                image(goblin) {
+                mobs.add(image(goblin) {
                     xy(g.x, g.y)
-                }
+                })
             }
 
             // Portal
-            val portalsMap = SpriteAnimation(resourcesVfs["images\\portals.png"].readBitmap(),
-                    spriteWidth = 64,
-                    spriteHeight = 64,
-                    marginLeft = 0,
-                    marginTop = 0,
-                    columns = 3,
-                    rows = 1,
-                    offsetBetweenColumns = 0,
-                    offsetBetweenRows = 0)
-            val portal = sprite(portalsMap).xy(MapParser.portal.x, MapParser.portal.y)
+            portal.xy(MapParser.portal.x, MapParser.portal.y)
             portal.spriteDisplayTime = 150.milliseconds
             portal.playAnimationLooped()
+            addChild(portal)
 
             // Player
             sprite.scale = 1.0
             addChild(sprite.xy(MapParser.player.x, MapParser.player.y))
         }
         if (!MainModule.dynamicLoad) load.await()
-
+        sprite.onCollision {
+            if (it == portal) {
+                if (views.input.keys[Key.E]) {
+                    launch { sceneContainer.changeTo<MainMenu>() }
+                }
+            }
+        }
+        sprite.
             addHrUpdater {
                 val scale = if (it == 0.hrMilliseconds) 0.0 else (it / 16.666666.hrMilliseconds)
+                /*blockDown = false
+                blockLeft = false
+                blockRight = false
+                blockUp = false
+                for (box in boxes) {
+                    if (box.collidesWith(sprite)) {
+                        println("" + sprite.x + ", " + sprite.y)
+                        println(box.pos)
+                        if (sprite.y + sprite.height + speed <= box.y
+                                && box.y >= sprite.y + sprite.height) blockDown = true
+                        if (box.y + box.height >= sprite.y
+                                && box.y + box.height <= sprite.y + speed) blockUp = true
+                        if (box.x <= sprite.x + sprite.width) blockRight = true
+                        if (box.x + box.width >= sprite.x) blockLeft = true
+                    }
+                }*/
                 when {
-                    views.input.keys[Key.RIGHT] -> {
+                    views.input.keys[Key.RIGHT] && !blockRight -> {
                         dx = this@Map.speed
                         dy = 0.0
                     }
-                    views.input.keys[Key.LEFT] -> {
+                    views.input.keys[Key.LEFT] && !blockLeft -> {
                         dx = -this@Map.speed
                         dy = 0.0
                     }
-                    views.input.keys[Key.UP] -> {
+                    views.input.keys[Key.UP] && !blockUp -> {
                         dy = -this@Map.speed
                         dx = 0.0
                     }
-                    views.input.keys[Key.DOWN] -> {
+                    views.input.keys[Key.DOWN] && !blockDown -> {
                         dy = this@Map.speed
                         dx = 0.0
                     }
@@ -108,8 +161,10 @@ class Map(val dependency: Dependency) : Scene() {
                         dx = 0.0
                     }
                 }
-                sprite.xy(sprite.x + dx * scale, sprite.y + dy * scale)
-                position(w/2 - sprite.x, h/2 - sprite.y)
+                curSpeed.x = dx * scale
+                curSpeed.y = dy * scale
+                xy(x + curSpeed.x, y + curSpeed.y)
+                this@sceneInit.position(w/2 - x, h/2 - y)
             }
     }
 }
